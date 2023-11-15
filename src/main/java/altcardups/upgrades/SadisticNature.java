08ae1a7cd38158d16207f5073abba3ec8e9ecae9
Ocean <ocean@ocean.lol>
2023-11-15 13:35:33 +0000

    tweaks (pommel strike was not that complicated)

--- src/main/java/altcardups/upgrades/SadisticNature.java
+++ src/main/java/altcardups/upgrades/SadisticNature.java
@@ -1,13 +1,6 @@
 package altcardups.upgrades;
 
-import altcardups.upgrades.interfaces.UseOverride;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
-import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.core.AbstractCreature;
-import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 public class SadisticNature extends AbstractAlternateUpgrade {
     public SadisticNature() {
--- src/main/java/altcardups/upgrades/ShrugItOff.java
+++ src/main/java/altcardups/upgrades/ShrugItOff.java
@@ -1,12 +1,9 @@
 package altcardups.upgrades;
 
 import altcardups.upgrades.interfaces.UseOverride;
-import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.DamageAction;
 import com.megacrit.cardcrawl.actions.common.DrawCardAction;
 import com.megacrit.cardcrawl.actions.common.GainBlockAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
-import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
@@ -16,12 +13,12 @@ public class ShrugItOff extends AbstractAlternateUpgrade implements UseOverride 
     }
     
     public void up(AbstractCard c) {
-        uBlock(c, 1);
+        setMagic(c, 2);
         uDesc(c);
     }
 
     public void use(AbstractCard c, AbstractPlayer p, AbstractMonster m) {
         atb(new GainBlockAction(p, c.block));
-        atb(new DrawCardAction(2));
+        atb(new DrawCardAction(c.magicNumber));
     }
 }
